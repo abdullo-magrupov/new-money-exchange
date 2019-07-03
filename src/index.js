52d7e7ff3f0f10d9fb7615cfc4f5204efb225b7f
abdullo-magrupov <magrupov.abdullo@gmail.com>
2019-07-03 17:57:47 +0500

    all tests successfully passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+  // Initial check for valid input
+  if (currency <= 0) {
+    return {};
+  } else if (currency > 10000) {
+    return {
+      error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+  }
+
+  // Reusable function for coin calculation
+  // Automatically populates result object and decreases currency amount
+  function calcCoins(coin) {
+    let coinCount = Math.floor(currency / coin.coinAmount);
+
+    if (coinCount > 0) {
+      currency = currency - coinCount * coin.coinAmount;
+      result[coin.coinCode] = coinCount;
+    }
+  }
+
+  let result = {};
+  let coins = [
+    { coinCode: "H", coinAmount: 50 },
+    { coinCode: "Q", coinAmount: 25 },
+    { coinCode: "D", coinAmount: 10 },
+    { coinCode: "N", coinAmount: 5 },
+    { coinCode: "P", coinAmount: 1 }
+  ];
+
+  // Making calculations for each coin
+  for (i = 0; i < coins.length; i++) {
+    calcCoins(coins[i]);
+  }
+
+  return result;
+};
